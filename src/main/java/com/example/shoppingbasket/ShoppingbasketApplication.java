7fc46211c4531004f831f4b8317c9134a7c19bde
Maximilian Mill <maximilianmuhlenberg@gmail.com>
2021-11-23 10:23:33 +0100

    Tried some Requests

--- src/main/java/com/example/shoppingbasket/ShoppingbasketApplication.java
+++ src/main/java/com/example/shoppingbasket/ShoppingbasketApplication.java
@@ -1,16 +1,21 @@
 package com.example.shoppingbasket;
 
 import com.example.shoppingbasket.item.Item;
+import com.example.shoppingbasket.item.ItemConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ShoppingbasketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingbasketApplication.class, args);
+
 	}
 
 }
